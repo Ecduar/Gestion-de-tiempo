7996d11c187cad85574c0d167e66f61bb1d0f92a
ecduar <estradanietoe@uvm.edu.ve>
2025-03-18 23:40:26 -0400

    Configuracion de la base de datos en el proyecto

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('./db');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
